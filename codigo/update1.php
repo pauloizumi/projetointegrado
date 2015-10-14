453b395df30a3e5689e695008d31f76015a4e2d3
pauloizumi <smariak2@hotmail.com>
2015-10-14 20:52:51 -0300

    entrega do dia 14

--- codigo/update1.php
+++ codigo/update1.php
@@ -8,25 +8,25 @@ $dbname = "manga";
 $vol1 = $_POST["vol1"];
 $vol = $_POST["vol"];
 
-// Abrindo conexão com o db.
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Verificação de conexão
-if ($conn->connect_error) {
+
+	if ($conn->connect_error) {
     die("Falha ao conectar: " . $conn->connect_error);
-} 
+	} 
 
 $sql = "UPDATE `volumes` set volume ='$vol1' WHERE volume = $vol";
 
-if ($conn->query($sql) === TRUE) {
+	if ($conn->query($sql) === TRUE) {
     echo "Dados salvos com sucesso!";
-} else {
+	}else {
     echo "Falha ao salvar: " . $conn->error;
-}
+	}	
 
 $conn->close();
 
-//aqui você pode colocar um header para te direcionar para outra página, sintaxe abaixo:
+
 header('location:volumes.php');
-//exit;	
+
 			
 ?>
